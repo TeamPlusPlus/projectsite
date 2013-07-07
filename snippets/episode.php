b8a11f32b84d00f8c9464e56585f226b9aa6aae8
Lukas Bestle <lukas@lu-x.me>
2013-07-07 17:10:32 +0200

    Add Disqus

--- snippets/episode.php
+++ snippets/episode.php
@@ -49,6 +49,21 @@
 					<div class="shownotes">
 					<?php echo kirbytext($p->shownotes()); ?>
 					</div>
+					
+					<hr>
+					<div id="disqus_thread"></div>
+					<script type="text/javascript">
+    				var disqus_shortname = '<?php echo $site->subdomain() ?>plusplus';
+    				var disqus_identifier = '<?php echo $p->uri(); ?>';
+    				var disqus_title = '<?php echo Episodes::title($p, 4); ?>';
+    				var disqus_url = '<?php echo $p->url(); ?>';
+    				
+    				(function() {
+        			var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        			dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    				})();
+					</script>
 					<?php } ?>
 					<?php } ?>
 				</section>
